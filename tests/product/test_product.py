c5bc2b0c7d4a4af60017e298402302df74d45f21
Queite <queitesc@gmail.com>
2022-10-28 15:25:48 -0300

    test: :white_check_mark: add Product class attributes test

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_data = [
+        2,
+        "Product Name",
+        "Company Name",
+        "2020-12-25",
+        "2022-12-02",
+        "123456789",
+        "keep in cool place",
+    ]
+
+    product = Product(*product_data)
+
+    assert product.id == product_data[0]
+    assert product.nome_do_produto == product_data[1]
+    assert product.nome_da_empresa == product_data[2]
+    assert product.data_de_fabricacao == product_data[3]
+    assert product.data_de_validade == product_data[4]
+    assert product.numero_de_serie == product_data[5]
+    assert product.instrucoes_de_armazenamento == product_data[6]
