ee694f3f9e65370d67558724c07e4e13e9f9e31c
Queite <queitesc@gmail.com>
2022-10-28 15:38:24 -0300

    test: :white_check_mark: add product class return test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    expected_return = (
+        "O produto Product Name fabricado em 2020-12-25 "
+        "por Company Name com validade até 2022-12-02 "
+        "precisa ser armazenado keep in cool place."
+    )
+
+    product = Product(
+        2,
+        "Product Name",
+        "Company Name",
+        "2020-12-25",
+        "2022-12-02",
+        "123456789",
+        "keep in cool place",
+    )
+
+    assert str(product) == expected_return
