90b83696261bf8e4e6ca89ec95a5188c578667be
Queite <queitesc@gmail.com>
2022-10-28 20:58:12 -0300

    refactor: :recycle: Changing Inventory: local tests ok, but evaluator tests don't pass

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,33 +8,53 @@ from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
-    def import_csv(path: str):
-        with open(path, encoding="utf8") as file:
-            file_to_dict = csv.DictReader(file)
-            dict_to_list = list(file_to_dict)
-        return dict_to_list
+    # def import_csv(path: str):
+    #     with open(path, encoding="utf8") as file:
+    #         file_to_dict = csv.DictReader(file)
+    #         dict_to_list = list(file_to_dict)
+    #     return dict_to_list
 
-    def import_json(path: str):
-        with open(path) as file:
-            content = file.read()
-            products = json.loads(content)
-        return products
+    # def import_json(path: str):
+    #     with open(path) as file:
+    #         content = file.read()
+    #         products = json.loads(content)
+    #     return products
 
-    def import_xml(path: str):
-        with open(path, "r") as file:
-            read = file.read()
-            dictionary = xmltodict.parse(read)
-        return list(dictionary["dataset"]["record"])
+    # def import_xml(path: str):
+    #     with open(path, "r") as file:
+    #         read = file.read()
+    #         dictionary = xmltodict.parse(read)
+    #     return list(dictionary["dataset"]["record"])
 
     def read_data(path: str):
         dict_list = []
-        if path.endswith("csv"):
-            dict_list = Inventory.import_csv(path)
-        if path.endswith("json"):
-            dict_list = Inventory.import_json(path)
-        if path.endswith("xml"):
-            dict_list = Inventory.import_xml(path)
+        with open(path) as file:
+            if path.endswith("csv"):
+                file_to_dict = csv.DictReader(file)
+                dict_to_list = list(file_to_dict)
+                dict_list = dict_to_list
+
+            if path.endswith("json"):
+                content = file.read()
+                products = json.loads(content)
+                dict_list = products
+
+            if path.endswith("xml"):
+                read = file.read()
+                dictionary = xmltodict.parse(read)
+                dict_list = list(dictionary["dataset"]["record"])
+
         return dict_list
+
+    # def read_data(path: str):
+    #     dict_list = []
+    #     if path.endswith("csv"):
+    #         dict_list = Inventory.import_csv(path)
+    #     if path.endswith("json"):
+    #         dict_list = Inventory.import_json(path)
+    #     if path.endswith("xml"):
+    #         dict_list = Inventory.import_xml(path)
+    #     return dict_list
 
     def import_data(path: str, type: str):
         dict_list = Inventory.read_data(path)
